b27285ea9309a4bbfa40e9b791725e957580755e
Nick Hofmann <nickhofmann1989@gmail.com>
2025-07-27 22:12:23 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,18 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();  // We will not be using this in this course.  -Nick
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options => options.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 // if (app.Environment.IsDevelopment())
 // {
 //     app.MapOpenApi();  // We will not be using this in this course.  -Nick
